f9562f1d2fecce75141595288d3739436f45301d
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-09-29 20:41:25 +0530

    Added the planet model

---
+++ src/models/planet.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const planetSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    climate: {
+        type: String,
+        required: true
+    },
+    terrain: {
+        type: String,
+        required: true
+    },
+    population: {
+        type: String,
+        required: true
+    },
+    orbital_period: {
+        type: String,
+        required: true
+    },
+    rotation_period: {
+        type: String,
+        required: true
+    },
+    diameter: {
+        type: String,
+        required: true
+    },
+    gravity: {
+        type: String,
+        required: true
+    },
+    surface_water: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps:true
+});
+
+export const Planet= mongoose.model("Planet", planetSchema);
