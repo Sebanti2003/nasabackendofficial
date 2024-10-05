b6eb5af728736621e61c33638e20489d9999a6f5
Sebanti2003 <dasguptasebanti2003@gmail.com>
2024-10-05 18:26:12 +0530

    Added the kep data routes models and controllers

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import { configDotenv } from 'dotenv';
 import connectDB from './db/index.js';
 import testrouter from './routes/test.route.js'
+import planetrouter from './routes/planets.route.js'
 const app=express();
 
 //DATABASE CONNECTION
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use('/api/v1/planets',planetrouter);
 app.use('/api/v1/test',testrouter)
 
 export default app;
